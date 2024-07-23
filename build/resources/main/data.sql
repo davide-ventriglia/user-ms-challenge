6151dd6f6cd416179d130fbe1da50f709759b007
Davide Ventriglia <davide.ventriglia.swd@gmail.com>
2024-07-23 16:25:24 +0200

    fixed gitignore, removed build folders

--- build/resources/main/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (first_name, last_name, email, address, insert_date, update_date) VALUES
-('Giovanni', 'Mucciaccia', 'giovannimucciaccia@gmail.com', 'Via Roma 1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Pippo', 'Baudo', 'pippobaudo@gmail.com', 'Via Milano 2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Gigi', 'Marzullo', 'gigimarzullo@gmail.com', 'Via Napoli 3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
